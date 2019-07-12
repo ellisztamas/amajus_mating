5ea240c9c6af8ed3f8538ff129acf2e5b3a065ae
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2019-07-12 15:01:54 +0200

    Updated raw_data_formatting.R sections dealing with genotype data to pull raw data from Seafile rather than my hard drive.

--- R/raw_data_format.R
+++ R/raw_data_format.R
@@ -23,7 +23,7 @@ gps <- as2012[,c("PlantID_final","Easting", "Northing", "Altitude")]
 # Centre Easting and Northing
 gps$Easting  <- gps$Easting - mean(gps$Easting)
 gps$Northing <- gps$Northing - mean(gps$Northing)
-write.csv(gps, 'data/GPS_positions.csv', row.names = F)
+write.csv(gps, 'data_processed//GPS_positions.csv', row.names = F)
 
 sum(as2012$phenoCat_final == -9) # there are 47 plants with no phenotype data
 # J1246 donated seed, but has a missing colour phenotype. I can fill this in from my field notes.
@@ -48,11 +48,11 @@ ros_sulf <- cbind(
 # sum(is.na(as2012$sulf)) # 370 plants not assigned.
 # # write to disk
 # ros_sulf <- data.frame(id = as2012$PlantID_final, Ros = as2012$rosea, Sulf=as2012$sulf)
-write.csv(ros_sulf, 'data/rosea_sulfurea.csv', row.names = F)
+write.csv(ros_sulf, 'data_processed/rosea_sulfurea.csv', row.names = F)
 
 # import offspring data. They are in two files from separate typing efforts
-offs1 <- read.csv('data/offspring_SNP_raw_data_1.csv', stringsAsFactors = F)
-offs2 <- read.csv('data/offspring_SNP_raw_data_2.csv', stringsAsFactors = F)
+offs1 <- read.csv('data_raw/offspring_SNP_raw_data_1.csv', stringsAsFactors = F)
+offs2 <- read.csv('data_raw/offspring_SNP_raw_data_2.csv', stringsAsFactors = F)
 # IDs match
 all(offs1$ID == offs2$ID)
 # bind into a single data frame.
@@ -71,14 +71,16 @@ offs <- offs[! offs$mothers %in% c("K2063", "M0065", "L0803.7"),]
 mx <- sapply(offs$mothers, function(y) which(sapply(strsplit(as2012$RametIDs, ","), function(x) y %in% x)))
 offs$mothers <- as2012$PlantID_final[mx]
 
+# The offspring SNP data have outdated names.
+# The new ones are in David's summary table of SNPs
+snp_summary <- read.csv('/datadisk/Seafile/HZanalyses/data/base/genetic/shortSummarySNPsAdj.csv', stringsAsFactors = F)
 # line up SNP names between parents and offspring
-snp_summary <- read.csv('data/shortSummarySNPsAdj.csv', stringsAsFactors = F)
 colnames(offs) <- c("ID", na.omit(snp_summary$LocusName[match(colnames(offs), snp_summary$LocusNameOLD)]), "mothers")
-
+# Get a column of parent genotypes in the same order as the offspring genotypes
 px <- intersect(colnames(offs), colnames(as2012)) # common set of SNPs.
 parents <- lgc2faps(as2012[,c("PlantID_final",px)], "/")
 offs <- offs[,c("ID","mothers",px)]
 
 # write genotypes to disk.
-write.csv(parents, file="data/parents_2012_genotypes.csv", row.names = F)
-write.csv(offs,    file="data/offspring_2012_genotypes.csv", row.names = F)
+write.csv(parents, file="data_processed/parents_2012_genotypes.csv", row.names = F)
+write.csv(offs,    file="data_processed/offspring_2012_genotypes.csv", row.names = F)
