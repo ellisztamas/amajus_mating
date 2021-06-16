2e513ba36b3627b727ec94b0fed80ec276845e3c
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2021-06-16 14:42:00 +0200

    Removed parallel loop for MCMC chains.
    joblib returned a bunch of errors about pickling that I don't want to
    attempt to fix, so I just switched to running 4 chains as a loop.
    
    On branch mcmc-restrict-kurtosis
     Changes to be committed:
    	modified:   005.results/003_mcmc_restrict_kurtosis/run_MCMC.py

--- 005.results/003_mcmc_restrict_kurtosis/run_MCMC.py
+++ 005.results/003_mcmc_restrict_kurtosis/run_MCMC.py
@@ -10,9 +10,7 @@ shape is between 1 and 3).
 import numpy as np
 import os
 from scipy.stats import beta
-from scipy.stats import gamma as gma
-from joblib import Parallel, delayed
-import multiprocessing
+from scipy.stats import gamma
 
 from amajusmating import mcmc
 
@@ -22,9 +20,10 @@ exec(open('003.scripts/setup_FAPS_GPS.py').read())
 # INITIALISE THE MODEL
 nreps = 3500 # Total number of iterations to run
 thin  = 10 # How often to write samples.
-max_distance = np.inf
+max_distance = np.inf # set a maximum dispersal distance
 # output_dir = "005.results/003_mcmc_restrict_kurtosis/output/"
 output_dir = os.path.dirname(os.path.abspath(__file__))+'/output/'
+os.makedirs(output_dir, exist_ok=True)
 
 np.random.seed(46)
 seeds = np.random.randint(1e4, size=len(chains))
@@ -33,8 +32,8 @@ seeds = np.random.randint(1e4, size=len(chains))
 priors = (lambda x : {
     'missing' : beta.pdf(x['missing'], a=3,   b=15),
     'mixture' : beta.pdf(x['mixture'], a=1.1, b=1.1),
-    'shape'   : gma.pdf(x['shape'],   a=10,  scale = 1/5),
-    'scale'   : gma.pdf(x['scale'],   a=6,   scale = 50)
+    'shape'   : gamma.pdf(x['shape'],   a=10,  scale = 1/5),
+    'scale'   : gamma.pdf(x['scale'],   a=6,   scale = 50)
 })
 
 # Proposed values are a Gaussian peturbation away from the previous values.
@@ -46,17 +45,15 @@ proposal_sigma = {
     'mixture' : 0.025,
 }
 
-# CHAIN A
-def single_chain(i, random_state):
-    # np.random.seed(seeds[i])
-    rng = np.random.RandomState(random_state)
+chains = [1,2,3,4]
+for i in chains:
     mcmc.run_MCMC(
         data= am_data,
         initial_parameters = {
-            'missing' : rng.beta(a=3, b = 15),
-            'shape'   : rng.gamma(shape=10,  scale = 1/5),
-            'scale'   : rng.gamma(shape=6,  scale = 50),
-            'mixture' : rng.beta(a=1.1, b = 1.1)
+            'missing' : beta.rvs(a=3, b = 15),
+            'shape'   : gamma.rvs(a=10,  scale = 1/5),
+            'scale'   : gamma.rvs(a=6,  scale = 50),
+            'mixture' : beta.rvs(a=1.1, b = 1.1)
         },
         proposal_sigma = proposal_sigma,
         priors = priors,
@@ -66,43 +63,3 @@ def single_chain(i, random_state):
         chain_name = 'chain' + str(i),
         max_distance = max_distance
         )
-
-chains = [1,2,3,4]
-num_cores = multiprocessing.cpu_count() -1
-Parallel(n_jobs=num_cores)(delayed(single_chain)(i, seeds[i-1]) for i in chains)
-
-# # CHAIN B
-# mcmc.run_MCMC(
-#     data= am_data,
-#     initial_parameters = {
-#         'missing' : beta.rvs(a=3, b = 15),
-#         'shape'  : gma.rvs(a=10,  scale = 1/5),
-#         'scale'  : gma.rvs(a=6,  scale = 50),
-#         'mixture' : beta.rvs(a=1.1, b = 1.1)
-#     },
-#     proposal_sigma = proposal_sigma,
-#     priors = priors,
-#     thin=thin,
-#     nreps=nreps,
-#     output_dir = os.path.dirname(os.path.abspath(__file__))+'/output/',
-#     chain_name = os.path.splitext(os.path.basename(__file__))[0],
-#     max_distance = max_distance
-#     )
-
-# # CHAIN C
-# mcmc.run_MCMC(
-#     data= am_data,
-#     initial_parameters = {
-#         'missing' : beta.rvs(a=3, b = 15),
-#         'shape'  : gma.rvs(a=10,  scale = 1/5),
-#         'scale'  : gma.rvs(a=6,  scale = 50),
-#         'mixture' : beta.rvs(a=1.1, b = 1.1)
-#     },
-#     proposal_sigma = proposal_sigma,
-#     priors = priors,
-#     thin=thin,
-#     nreps=nreps,
-#     output_dir = os.path.dirname(os.path.abspath(__file__))+'/output/',
-#     chain_name = os.path.splitext(os.path.basename(__file__))[0],
-#     max_distance = max_distance
-#     )
